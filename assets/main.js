6905db6f26f634d380ba40713592414f0dfb1164
Adam <adamvhayward@hotmail.com>
2019-10-10 15:11:23 -0400

    update

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,8 @@
 "use strict";
 
 function getParks() {
-let userValue = $(".parkValue").val();
-const url = `https://developer.nps.gov/api/v1/parks?stateCode=${userValue}&api_key=ugQP2Zmj80Rp0eobwIE0QKWhUqk95p44B6q3ZFJ1`;
-  fetch(url)
+let userValue = $("#parkValue").val();
+  fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${userValue}&api_key=ugQP2Zmj80Rp0eobwIE0QKWhUqk95p44B6q3ZFJ1`)
   .then(response => response.json()
   .then(responseJson => console.log(responseJson))
   .catch( error => console.log('Didn"t log it '))
@@ -17,6 +16,6 @@ function clickForm() {
   });
 }
 
-$(function() {
-  getParks();
+$(function(){
+    clickForm();
 });
